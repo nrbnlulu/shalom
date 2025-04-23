d01b414a0fa0528009d505ac3f93964695599d37
bendeez <daniellacina8@gmail.com>
2025-04-23 09:33:54 -0500

    added more test cases

---
+++ rust/shalom_dart_codegen/dart_tests/test/enum_selection/__graphql__/GetTaskWithUndecisiveStatus.shalom.dart
@@ -0,0 +1,209 @@
+typedef JsonObject = Map<String, dynamic>;
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: camel_case_types
+
+class RequestGetTaskWithUndecisiveStatus {
+  /// class members
+
+  final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus?
+  taskWithUndecisiveStatus;
+
+  // keywordargs constructor
+
+  RequestGetTaskWithUndecisiveStatus({this.taskWithUndecisiveStatus});
+  static RequestGetTaskWithUndecisiveStatus fromJson(JsonObject data) {
+    final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus?
+    taskWithUndecisiveStatus_value;
+
+    final JsonObject? taskWithUndecisiveStatus$raw =
+        data['taskWithUndecisiveStatus'];
+    if (taskWithUndecisiveStatus$raw != null) {
+      taskWithUndecisiveStatus_value =
+          GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus.fromJson(
+            taskWithUndecisiveStatus$raw,
+          );
+    } else {
+      taskWithUndecisiveStatus_value = null;
+    }
+
+    return RequestGetTaskWithUndecisiveStatus(
+      taskWithUndecisiveStatus: taskWithUndecisiveStatus_value,
+    );
+  }
+
+  RequestGetTaskWithUndecisiveStatus updateWithJson(JsonObject data) {
+    final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus?
+    taskWithUndecisiveStatus_value;
+    if (data.containsKey('taskWithUndecisiveStatus')) {
+      final JsonObject? taskWithUndecisiveStatus$raw =
+          data['taskWithUndecisiveStatus'];
+      if (taskWithUndecisiveStatus$raw != null) {
+        taskWithUndecisiveStatus_value =
+            GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus.fromJson(
+              taskWithUndecisiveStatus$raw,
+            );
+      } else {
+        taskWithUndecisiveStatus_value = null;
+      }
+    } else {
+      taskWithUndecisiveStatus_value = taskWithUndecisiveStatus;
+    }
+
+    return RequestGetTaskWithUndecisiveStatus(
+      taskWithUndecisiveStatus: taskWithUndecisiveStatus_value,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is RequestGetTaskWithUndecisiveStatus &&
+            other.taskWithUndecisiveStatus == taskWithUndecisiveStatus &&
+            true);
+  }
+
+  @override
+  int get hashCode => taskWithUndecisiveStatus.hashCode;
+
+  JsonObject toJson() {
+    return {'taskWithUndecisiveStatus': taskWithUndecisiveStatus?.toJson()};
+  }
+}
+
+// ------------ OBJECT DEFINITIONS -------------
+
+class GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus {
+  /// class members
+
+  final String id;
+
+  final String name;
+
+  final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status? status;
+
+  // keywordargs constructor
+  GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus({
+    required this.id,
+    required this.name,
+
+    this.status,
+  });
+  static GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus fromJson(
+    JsonObject data,
+  ) {
+    final String id_value = data['id'];
+
+    final String name_value = data['name'];
+
+    final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status?
+    status_value;
+
+    final String? status$raw = data['status'];
+    if (status$raw != null) {
+      status_value =
+          GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.fromString(
+            status$raw,
+          );
+    } else {
+      status_value = null;
+    }
+
+    return GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus(
+      id: id_value,
+
+      name: name_value,
+
+      status: status_value,
+    );
+  }
+
+  GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus updateWithJson(
+    JsonObject data,
+  ) {
+    final String id_value;
+    if (data.containsKey('id')) {
+      id_value = data['id'];
+    } else {
+      id_value = id;
+    }
+
+    final String name_value;
+    if (data.containsKey('name')) {
+      name_value = data['name'];
+    } else {
+      name_value = name;
+    }
+
+    final GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status?
+    status_value;
+    if (data.containsKey('status')) {
+      final String? status$raw = data['status'];
+      if (status$raw != null) {
+        status_value =
+            GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.fromString(
+              status$raw,
+            );
+      } else {
+        status_value = null;
+      }
+    } else {
+      status_value = status;
+    }
+
+    return GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus(
+      id: id_value,
+
+      name: name_value,
+
+      status: status_value,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus &&
+            other.id == id &&
+            other.name == name &&
+            other.status == status &&
+            true);
+  }
+
+  @override
+  int get hashCode => Object.hashAll([id, name, status]);
+
+  JsonObject toJson() {
+    return {'id': id, 'name': name, 'status': status?.toString()};
+  }
+}
+
+// ------------ Enum DEFINITIONS -------------
+
+enum GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status {
+  COMPLETED,
+
+  FAILED,
+
+  PENDING,
+
+  PROCESSING;
+
+  static GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status fromString(
+    String value,
+  ) {
+    return GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.values
+        .firstWhere(
+          (e) => e.name.toUpperCase() == value.toUpperCase(),
+          orElse:
+              () =>
+                  throw ArgumentError(
+                    "Unknown GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status $value",
+                  ),
+        );
+  }
+
+  @override
+  String toString() => name.toUpperCase();
+}
+
+// ------------ END Enum DEFINITIONS -------------
--- rust/shalom_dart_codegen/dart_tests/test/enum_selection/test.dart
+++ rust/shalom_dart_codegen/dart_tests/test/enum_selection/test.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
 import "__graphql__/GetTask.shalom.dart";
+import "__graphql__/GetTaskWithUndecisiveStatus.shalom.dart";
 
 void main() {
   group('Test enum selection', () {
@@ -35,6 +36,129 @@ void main() {
       final updated = initial.updateWithJson({'task': taskJson});
       expect(updated.task?.status, GetTask_task_status.COMPLETED);
       expect(initial, isNot(updated));
+    });
+  });
+  group('test optional enum selection', () {
+    group("deserialize", () {
+      test('with value', () {
+        final json = {
+          "taskWithUndecisiveStatus": {
+            "id": "foo",
+            "name": "do nothing",
+            "status": "FAILED",
+          },
+        };
+        final result = RequestGetTaskWithUndecisiveStatus.fromJson(json);
+        expect(result.taskWithUndecisiveStatus?.id, "foo");
+        expect(result.taskWithUndecisiveStatus?.name, "do nothing");
+        expect(
+          result.taskWithUndecisiveStatus?.status,
+          GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.FAILED,
+        );
+      });
+      test('null value', () {
+        final json = {
+          "taskWithUndecisiveStatus": {
+            "id": "foo",
+            "name": "do nothing",
+            "status": null,
+          },
+        };
+        final result = RequestGetTaskWithUndecisiveStatus.fromJson(json);
+        expect(result.taskWithUndecisiveStatus?.id, "foo");
+        expect(result.taskWithUndecisiveStatus?.name, "do nothing");
+        expect(result.taskWithUndecisiveStatus?.status, null);
+      });
+    });
+    group("serialize", () {
+      test('with value', () {
+        final data = {
+          "taskWithUndecisiveStatus": {
+            "id": "foo",
+            "name": "do nothing",
+            "status": "FAILED",
+          },
+        };
+        final initial = RequestGetTaskWithUndecisiveStatus.fromJson(data);
+        final json = initial.toJson();
+        expect(json, data);
+      });
+      test('null value', () {
+        final data = {
+          "taskWithUndecisiveStatus": {
+            "id": "foo",
+            "name": "do nothing",
+            "status": null,
+          },
+        };
+        final initial = RequestGetTaskWithUndecisiveStatus.fromJson(data);
+        final json = initial.toJson();
+        expect(json, data);
+      });
+    });
+    group("update", () {
+      test("null to some", () {
+        final initial = RequestGetTaskWithUndecisiveStatus(
+          taskWithUndecisiveStatus:
+              GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus(
+                id: "foo",
+                name: "do nothing",
+                status: null,
+              ),
+        );
+        final taskJson = initial.taskWithUndecisiveStatus?.toJson();
+        taskJson?["status"] = "COMPLETED";
+        final updated = initial.updateWithJson({
+          'taskWithUndecisiveStatus': taskJson,
+        });
+        expect(
+          updated.taskWithUndecisiveStatus?.status,
+          GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.COMPLETED,
+        );
+        expect(initial, isNot(updated));
+      });
+
+      test("some to some", () {
+        final initial = RequestGetTaskWithUndecisiveStatus(
+          taskWithUndecisiveStatus:
+              GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus(
+                id: "foo",
+                name: "do nothing",
+                status:
+                    GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status
+                        .FAILED,
+              ),
+        );
+        final taskJson = initial.taskWithUndecisiveStatus?.toJson();
+        taskJson?["status"] = "COMPLETED";
+        final updated = initial.updateWithJson({
+          'taskWithUndecisiveStatus': taskJson,
+        });
+        expect(
+          updated.taskWithUndecisiveStatus?.status,
+          GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status.COMPLETED,
+        );
+        expect(initial, isNot(updated));
+      });
+      test("some to null", () {
+        final initial = RequestGetTaskWithUndecisiveStatus(
+          taskWithUndecisiveStatus:
+              GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus(
+                id: "foo",
+                name: "do nothing",
+                status:
+                    GetTaskWithUndecisiveStatus_taskWithUndecisiveStatus_status
+                        .FAILED,
+              ),
+        );
+        final taskJson = initial.taskWithUndecisiveStatus?.toJson();
+        taskJson?["status"] = null;
+        final updated = initial.updateWithJson({
+          'taskWithUndecisiveStatus': taskJson,
+        });
+        expect(updated.taskWithUndecisiveStatus?.status, null);
+        expect(initial, isNot(updated));
+      });
     });
   });
 }
