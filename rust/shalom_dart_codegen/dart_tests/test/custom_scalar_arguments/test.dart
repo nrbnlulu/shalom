6d8022f04944445e05ac57da311ef5c868553778
uttkarshrastogi <50842869+uttkarshrastogi@users.noreply.github.com>
2025-06-23 12:17:49 +0530

    resolved issues----

--- rust/shalom_dart_codegen/dart_tests/test/custom_scalar_arguments/test.dart
+++ rust/shalom_dart_codegen/dart_tests/test/custom_scalar_arguments/test.dart
@@ -16,132 +16,141 @@ void main() {
   final Point updatedPoint = Point(x: 30, y: 40);
   final String updatedPointRaw = "POINT (30, 40)";
 
-  test("required custom scalar argument", () {
-    var variables = UpdatePointCoordsNonNullVariables(coords: samplePoint);
-    expect(variables.toJson(), {"coords": samplePointRaw});
-    var variablesUpdated = variables.updateWith(coords: updatedPoint);
-    expect(variablesUpdated.coords, updatedPoint);
-    final req =
-        RequestUpdatePointCoordsNonNull(
-          variables: UpdatePointCoordsNonNullVariables(coords: updatedPoint),
-        ).toRequest();
-    expect(req.variables, {"coords": updatedPointRaw});
-  });
+  group('custom scalar direct arguments', () {
+    test("required custom scalar argument", () {
+      var variables = UpdatePointCoordsNonNullVariables(coords: samplePoint);
+      expect(variables.toJson(), {"coords": samplePointRaw});
+      var variablesUpdated = variables.updateWith(coords: updatedPoint);
+      expect(variablesUpdated.coords, updatedPoint);
+      final req =
+          RequestUpdatePointCoordsNonNull(
+            variables: UpdatePointCoordsNonNullVariables(coords: updatedPoint),
+          ).toRequest();
+      expect(req.variables, {"coords": updatedPointRaw});
+    });
 
-  test("optional custom scalar argument", () {
-    var variables = UpdatePointCoordsMaybeVariables(coords: Some(samplePoint));
-    var variablesUpdated = variables.updateWith(
-      coords: Some(Some(updatedPoint)),
-    );
-    expect(variablesUpdated.coords.some(), updatedPoint);
-    expect(
-      RequestUpdatePointCoordsMaybe(
-        variables: UpdatePointCoordsMaybeVariables(coords: Some(null)),
-      ).toRequest().variables,
-      {"coords": null},
-    );
+    test("custom scalar (argument) maybe", () {
+      var variables = UpdatePointCoordsMaybeVariables(
+        coords: Some(samplePoint),
+      );
+      var variablesUpdated = variables.updateWith(
+        coords: Some(Some(updatedPoint)),
+      );
+      expect(variablesUpdated.coords.some(), updatedPoint);
+      expect(
+        RequestUpdatePointCoordsMaybe(
+          variables: UpdatePointCoordsMaybeVariables(coords: Some(null)),
+        ).toRequest().variables,
+        {"coords": null},
+      );
 
-    expect(
-      RequestUpdatePointCoordsMaybe(
-        variables: UpdatePointCoordsMaybeVariables(coords: Some(samplePoint)),
-      ).toRequest().variables,
-      {"coords": samplePointRaw},
-    );
-    expect(
-      RequestUpdatePointCoordsMaybe(
-        variables: UpdatePointCoordsMaybeVariables(coords: None()),
-      ).toRequest().variables,
-      {},
-    );
-  });
+      expect(
+        RequestUpdatePointCoordsMaybe(
+          variables: UpdatePointCoordsMaybeVariables(coords: Some(samplePoint)),
+        ).toRequest().variables,
+        {"coords": samplePointRaw},
+      );
+      expect(
+        RequestUpdatePointCoordsMaybe(
+          variables: UpdatePointCoordsMaybeVariables(coords: None()),
+        ).toRequest().variables,
+        {},
+      );
+    });
 
-  test("required custom scalar argument in InputObject", () {
-    final variables = UpdatePointWithInputNonNullVariables(
-      pointData: PointDataInput(coords: samplePoint, name: "Location A"),
-    );
-    final variablesUpdated = variables.updateWith(
-      pointData: PointDataInput(coords: updatedPoint, name: "Location B"),
-    );
-    expect(variablesUpdated.pointData.coords, updatedPoint);
+    test("custom scalar argument with (optional default value)", () {
+      final req =
+          RequestUpdatePointCoordsOpt(
+            variables: UpdatePointCoordsOptVariables(id: "test-id-1"),
+          ).toRequest();
+      expect(req.variables, {"id": "test-id-1", "coords": null});
 
-    final req =
-        RequestUpdatePointWithInputNonNull(
-          variables: UpdatePointWithInputNonNullVariables(
-            pointData: PointDataInput(
-              coords: Point(x: 50, y: 60),
-              name: "Location C",
+      final reqWithExplicit =
+          RequestUpdatePointCoordsOpt(
+            variables: UpdatePointCoordsOptVariables(
+              id: "test-id-2",
+              coords: Point(x: 15, y: 25),
             ),
-          ),
-        ).toRequest();
-    expect(req.variables, {
-      "pointData": {"coords": "POINT (50, 60)", "name": "Location C"},
+          ).toRequest();
+      expect(reqWithExplicit.variables, {
+        "id": "test-id-2",
+        "coords": "POINT (15, 25)",
+      });
     });
   });
 
-  test("optional custom scalar argument in InputObject", () {
-    final variables = UpdatePointWithInputCoordsMaybeVariables(
-      pointData: PointUpdateCoordsMaybe(coords: Some(null), name: "Location D"),
-    );
-    final variablesUpdated = variables.updateWith(
-      pointData: PointUpdateCoordsMaybe(
-        coords: Some(updatedPoint),
-        name: "Location E",
-      ),
-    );
-    expect(variablesUpdated.pointData.coords, Some<Point?>(updatedPoint));
-    final req =
-        RequestUpdatePointWithInputCoordsMaybe(
-          variables: variables,
-        ).toRequest();
-    expect(req.variables, {
-      "pointData": {"coords": null, "name": "Location D"},
-    });
-  });
+  group('custom scalar in InputObject', () {
+    test("custom scalar argument in InputObject (required)", () {
+      final variables = UpdatePointWithInputNonNullVariables(
+        pointData: PointDataInput(coords: samplePoint, name: "Location A"),
+      );
+      final variablesUpdated = variables.updateWith(
+        pointData: PointDataInput(coords: updatedPoint, name: "Location B"),
+      );
+      expect(variablesUpdated.pointData.coords, updatedPoint);
 
-  test("optional custom scalar argument with default value", () {
-    final req =
-        RequestUpdatePointCoordsOpt(
-          variables: UpdatePointCoordsOptVariables(id: "test-id-1"),
-        ).toRequest();
-    expect(req.variables, {"id": "test-id-1", "coords": null});
-
-    final reqWithExplicit =
-        RequestUpdatePointCoordsOpt(
-          variables: UpdatePointCoordsOptVariables(
-            id: "test-id-2",
-            coords: Point(x: 15, y: 25),
-          ),
-        ).toRequest();
-    expect(reqWithExplicit.variables, {
-      "id": "test-id-2",
-      "coords": "POINT (15, 25)",
-    });
-  });
-
-  test("optional custom scalar argument with default value in InputObject", () {
-    final variables = updatePointWithOptCoordsVariables(
-      pointData: PointDataOptCoordsInput(name: "Location M"),
-    );
-
-    final req =
-        RequestupdatePointWithOptCoords(variables: variables).toRequest();
-    expect(req.variables, {
-      "pointData": {"coords": null, "name": "Location M"},
+      final req =
+          RequestUpdatePointWithInputNonNull(
+            variables: UpdatePointWithInputNonNullVariables(
+              pointData: PointDataInput(
+                coords: Point(x: 50, y: 60),
+                name: "Location C",
+              ),
+            ),
+          ).toRequest();
+      expect(req.variables, {
+        "pointData": {"coords": "POINT (50, 60)", "name": "Location C"},
+      });
     });
 
-    final variablesWithCoords = updatePointWithOptCoordsVariables(
-      pointData: PointDataOptCoordsInput(
-        coords: Point(x: 25, y: 35),
-        name: "Location N",
-      ),
-    );
+    test("custom scalar in InputObject (maybe)", () {
+      final variables = UpdatePointWithInputCoordsMaybeVariables(
+        pointData: PointUpdateCoordsMaybe(
+          coords: Some(null),
+          name: "Location D",
+        ),
+      );
+      final variablesUpdated = variables.updateWith(
+        pointData: PointUpdateCoordsMaybe(
+          coords: Some(updatedPoint),
+          name: "Location E",
+        ),
+      );
+      expect(variablesUpdated.pointData.coords, Some<Point?>(updatedPoint));
+      final req =
+          RequestUpdatePointWithInputCoordsMaybe(
+            variables: variables,
+          ).toRequest();
+      expect(req.variables, {
+        "pointData": {"coords": null, "name": "Location D"},
+      });
+    });
 
-    final reqWithCoords =
-        RequestupdatePointWithOptCoords(
-          variables: variablesWithCoords,
-        ).toRequest();
-    expect(reqWithCoords.variables, {
-      "pointData": {"coords": "POINT (25, 35)", "name": "Location N"},
+    test("custom scalar InputObject (optional default value)", () {
+      final variables = updatePointWithOptCoordsVariables(
+        pointData: PointDataOptCoordsInput(name: "Location M"),
+      );
+
+      final req =
+          RequestupdatePointWithOptCoords(variables: variables).toRequest();
+      expect(req.variables, {
+        "pointData": {"coords": null, "name": "Location M"},
+      });
+
+      final variablesWithCoords = updatePointWithOptCoordsVariables(
+        pointData: PointDataOptCoordsInput(
+          coords: Point(x: 25, y: 35),
+          name: "Location N",
+        ),
+      );
+
+      final reqWithCoords =
+          RequestupdatePointWithOptCoords(
+            variables: variablesWithCoords,
+          ).toRequest();
+      expect(reqWithCoords.variables, {
+        "pointData": {"coords": "POINT (25, 35)", "name": "Location N"},
+      });
     });
   });
 }
