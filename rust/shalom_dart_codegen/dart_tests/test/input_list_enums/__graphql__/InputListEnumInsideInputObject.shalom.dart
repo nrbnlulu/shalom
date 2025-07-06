4c5125cc764f58bd7c22f48342cc37d2e53e74ec
ניר <88795475+nrbnlulu@users.noreply.github.com>
2025-07-06 16:53:15 +0300

    Nir/input/list of enum (#52)

---
+++ rust/shalom_dart_codegen/dart_tests/test/input_list_enums/__graphql__/InputListEnumInsideInputObject.shalom.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names, unused_import, camel_case_types
+
+import "schema.shalom.dart";
+
+import 'package:shalom_core/shalom_core.dart';
+
+typedef JsonObject = Map<String, dynamic>;
+
+class InputListEnumInsideInputObjectResponse {
+  /// class members
+
+  final String? InputListEnumInsideInputObject;
+
+  // keywordargs constructor
+  InputListEnumInsideInputObjectResponse({this.InputListEnumInsideInputObject});
+  static InputListEnumInsideInputObjectResponse fromJson(JsonObject data) {
+    final String? InputListEnumInsideInputObject_value;
+
+    InputListEnumInsideInputObject_value =
+        data['InputListEnumInsideInputObject'];
+
+    return InputListEnumInsideInputObjectResponse(
+      InputListEnumInsideInputObject: InputListEnumInsideInputObject_value,
+    );
+  }
+
+  InputListEnumInsideInputObjectResponse updateWithJson(JsonObject data) {
+    final String? InputListEnumInsideInputObject_value;
+    if (data.containsKey('InputListEnumInsideInputObject')) {
+      InputListEnumInsideInputObject_value =
+          data['InputListEnumInsideInputObject'];
+    } else {
+      InputListEnumInsideInputObject_value = InputListEnumInsideInputObject;
+    }
+
+    return InputListEnumInsideInputObjectResponse(
+      InputListEnumInsideInputObject: InputListEnumInsideInputObject_value,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is InputListEnumInsideInputObjectResponse &&
+            other.InputListEnumInsideInputObject ==
+                InputListEnumInsideInputObject);
+  }
+
+  @override
+  int get hashCode => InputListEnumInsideInputObject.hashCode;
+
+  JsonObject toJson() {
+    return {'InputListEnumInsideInputObject': InputListEnumInsideInputObject};
+  }
+}
+
+// ------------ OBJECT DEFINITIONS -------------
+
+// ------------ END OBJECT DEFINITIONS -------------
+
+class RequestInputListEnumInsideInputObject extends Requestable {
+  final InputListEnumInsideInputObjectVariables variables;
+
+  RequestInputListEnumInsideInputObject({required this.variables});
+
+  @override
+  Request toRequest() {
+    JsonObject variablesJson = variables.toJson();
+    return Request(
+      query:
+          r"""mutation InputListEnumInsideInputObject($input: ObjectWithListOfInput!) {
+  InputListEnumInsideInputObject(input: $input)
+}""",
+      variables: variablesJson,
+      opType: OperationType.Mutation,
+      StringopName: 'InputListEnumInsideInputObject',
+    );
+  }
+}
+
+class InputListEnumInsideInputObjectVariables {
+  final ObjectWithListOfInput input;
+
+  InputListEnumInsideInputObjectVariables({required this.input});
+
+  JsonObject toJson() {
+    JsonObject data = {};
+
+    data["input"] = input.toJson();
+
+    return data;
+  }
+
+  InputListEnumInsideInputObjectVariables updateWith({
+    ObjectWithListOfInput? input,
+  }) {
+    final ObjectWithListOfInput input$next;
+
+    if (input != null) {
+      input$next = input;
+    } else {
+      input$next = this.input;
+    }
+
+    return InputListEnumInsideInputObjectVariables(input: input$next);
+  }
+}
---
+++ rust/shalom_dart_codegen/dart_tests/test/input_list_enums/__graphql__/InputListEnumOptionalWithDefault.shalom.dart
@@ -0,0 +1,112 @@
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names, unused_import, camel_case_types
+
+import "schema.shalom.dart";
+
+import 'package:shalom_core/shalom_core.dart';
+
+typedef JsonObject = Map<String, dynamic>;
+
+class InputListEnumOptionalWithDefaultResponse {
+  /// class members
+
+  final String? InputListEnumOptionalWithDefault;
+
+  // keywordargs constructor
+  InputListEnumOptionalWithDefaultResponse({
+    this.InputListEnumOptionalWithDefault,
+  });
+  static InputListEnumOptionalWithDefaultResponse fromJson(JsonObject data) {
+    final String? InputListEnumOptionalWithDefault_value;
+
+    InputListEnumOptionalWithDefault_value =
+        data['InputListEnumOptionalWithDefault'];
+
+    return InputListEnumOptionalWithDefaultResponse(
+      InputListEnumOptionalWithDefault: InputListEnumOptionalWithDefault_value,
+    );
+  }
+
+  InputListEnumOptionalWithDefaultResponse updateWithJson(JsonObject data) {
+    final String? InputListEnumOptionalWithDefault_value;
+    if (data.containsKey('InputListEnumOptionalWithDefault')) {
+      InputListEnumOptionalWithDefault_value =
+          data['InputListEnumOptionalWithDefault'];
+    } else {
+      InputListEnumOptionalWithDefault_value = InputListEnumOptionalWithDefault;
+    }
+
+    return InputListEnumOptionalWithDefaultResponse(
+      InputListEnumOptionalWithDefault: InputListEnumOptionalWithDefault_value,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is InputListEnumOptionalWithDefaultResponse &&
+            other.InputListEnumOptionalWithDefault ==
+                InputListEnumOptionalWithDefault);
+  }
+
+  @override
+  int get hashCode => InputListEnumOptionalWithDefault.hashCode;
+
+  JsonObject toJson() {
+    return {
+      'InputListEnumOptionalWithDefault': InputListEnumOptionalWithDefault,
+    };
+  }
+}
+
+// ------------ OBJECT DEFINITIONS -------------
+
+// ------------ END OBJECT DEFINITIONS -------------
+
+class RequestInputListEnumOptionalWithDefault extends Requestable {
+  final InputListEnumOptionalWithDefaultVariables variables;
+
+  RequestInputListEnumOptionalWithDefault({required this.variables});
+
+  @override
+  Request toRequest() {
+    JsonObject variablesJson = variables.toJson();
+    return Request(
+      query:
+          r"""mutation InputListEnumOptionalWithDefault($foo: [Gender!] = null) {
+  InputListEnumOptionalWithDefault(foo: $foo)
+}""",
+      variables: variablesJson,
+      opType: OperationType.Mutation,
+      StringopName: 'InputListEnumOptionalWithDefault',
+    );
+  }
+}
+
+class InputListEnumOptionalWithDefaultVariables {
+  final List<Gender>? foo;
+
+  InputListEnumOptionalWithDefaultVariables({this.foo});
+
+  JsonObject toJson() {
+    JsonObject data = {};
+
+    data["foo"] = foo?.map((e) => e.name).toList();
+
+    return data;
+  }
+
+  InputListEnumOptionalWithDefaultVariables updateWith({
+    Option<List<Gender>?> foo = const None(),
+  }) {
+    final List<Gender>? foo$next;
+
+    switch (foo) {
+      case Some(value: final data):
+        foo$next = data;
+      case None():
+        foo$next = this.foo;
+    }
+
+    return InputListEnumOptionalWithDefaultVariables(foo: foo$next);
+  }
+}
