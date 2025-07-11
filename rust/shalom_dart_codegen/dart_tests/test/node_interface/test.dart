161a4e6688d96cca7f9150c3859d48ec40a5e8b4
Daniel Lacina <daniellacina8@gmail.com>
2025-07-10 20:47:00 -0500

    working on tests

--- rust/shalom_dart_codegen/dart_tests/test/node_interface/test.dart
+++ rust/shalom_dart_codegen/dart_tests/test/node_interface/test.dart
@@ -1,4 +1,19 @@
 import 'package:test/test.dart';
 import 'package:shalom_core/shalom_core.dart';
+import '__graphql__/GetUser.shalom.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-void main() {}
+void main() {
+    test("test sync nodes", () {
+        final manager = NodeManager();
+        final context = ShalomContext(manager: manager);
+        final id = "1";
+        final initialUserData = {"id": id, "name": "qtgql", "email": "qtgql@gmail.com", "age": 2};
+        final initialUserNode = GetUser_user.deserialize(initialUserData, context);
+        NodeWidget<GetUser_user>(node: initialUserNode, builder: (_, node) => SizedBox(width: 16), context: context);  
+        final nextUserData = {"id": id, "name": "shalom", "email": "shalom@gmail.com", "age": 1}; 
+        GetUser_user.deserialize(nextUserData, context);
+        print(initialUserNode.toJson());
+    });
+}
