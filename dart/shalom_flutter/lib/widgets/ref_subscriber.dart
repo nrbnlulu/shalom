04f569dc38dab09728439cebc1895f9443eaae9b
nir <nrbnlulu@gmail.com>
2025-09-08 23:58:42 +0300

    wip

--- dart/shalom_flutter/lib/widgets/ref_subscriber.dart
+++ dart/shalom_flutter/lib/widgets/ref_subscriber.dart
@@ -7,31 +7,38 @@ import 'package:shalom_core/shalom_core.dart';
 /// this is an internal widget and should prob not be used by users
 /// this would later on be used by the codegen to generate UseQuery / UseFragment widgets
 class RefSubscriberWidget<T> extends StatefulWidget {
-  final Widget child;
   final ShalomCtx shalomCtx;
   final Set<RecordRef> initialRefs;
-  final T? Function(JsonObject) parseData;
+  final T initialResult;
+  final T Function(List<RefUpdate>) onRefUpdate;
+  final Widget Function({BuildContext ctx, T data}) childBuilder;
   
   const RefSubscriberWidget({
     super.key,
+    required this.initialResult,
+    required this.onRefUpdate,
     required this.shalomCtx,
     required this.initialRefs,
-    required this.child,
+    required this.childBuilder
   });
 
   @override
-  State<RefSubscriberWidget> createState() => _RefSubscriberWidgetState();
+  State<RefSubscriberWidget<T>> createState() => _RefSubscriberWidgetState();
 }
 
-class _RefSubscriberWidgetState extends State<RefSubscriberWidget> {
+class _RefSubscriberWidgetState<T> extends State<RefSubscriberWidget<T>> {
   RefSubscriber? subscription;
+  late T data = widget.initialResult;
+  
   @override
   void initState() {
     super.initState();
     subscription = widget.shalomCtx.cache.subscribeToRefs(widget.initialRefs);
     subscription!.streamController.stream.listen(
         (update){
-            setState(() {});
+            setState(() {
+                data = widget.onRefUpdate(update);
+            });
         }
     )
   }
@@ -43,6 +50,9 @@ class _RefSubscriberWidgetState extends State<RefSubscriberWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return widget.child;
+      return widget.childBuilder(
+          ctx: context, data:data
+      );
+      
   }
 }
