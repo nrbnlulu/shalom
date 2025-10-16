81b7c2e87ee8992b42cf36eb2f907a7cee520e82
Ofir <nofatigua@gmail.com>
2025-10-16 18:14:38 +0300

    fix

--- rust/shalom_dart_codegen/tests/usecases_test.rs
+++ rust/shalom_dart_codegen/tests/usecases_test.rs
@@ -100,11 +100,6 @@ fn test_fragments_on_interface_dart() {
     run_dart_tests_for_usecase("fragments_on_interface");
 }
 #[test]
-fn test_input_list_with_defaults_dart() {
-    run_dart_tests_for_usecase("input_list_with_defaults");
-}
-
-#[test]
 fn test_object_fragment_list_interfaces_dart() {
     run_dart_tests_for_usecase("object_fragment_list_interfaces");
 
