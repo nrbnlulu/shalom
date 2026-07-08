9c0a086fad8869e2375a7b28838ace1938d690c7
nir benlulu <nrbnlulu@gmail.com>
2026-07-08 11:30:51 +0300

    Update shalom_flutter.dart

--- dart/shalom_flutter/lib/shalom_flutter.dart
+++ dart/shalom_flutter/lib/shalom_flutter.dart
@@ -1,6 +1,7 @@
 export 'widgets/shalom_provider.dart'
     show ShalomInheritedWidget, ShalomScope, ShalomContextExtension;
 export 'src/optimistic_mutation_response.dart';
+export 'src/observable_state_mixin.dart' show ShalomObservingState;
 export 'widgets/debug_panel.dart' show ShalomDebugPanel;
 export 'widgets/observable_scope.dart'
     show
